33db64b98420d8dd11b366de60c3391e13be8e29
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-05-22 19:03:22 -0500

    last
    last

--- preguntas.py
+++ preguntas.py
@@ -130,8 +130,8 @@ def pregunta_04():
     (X_train, X_test, y_train, y_test,) = train_test_split(
         X_fertility,
         y_life,
-        test_size=53,
-        random_state=80,
+        test_size=0.2,
+        random_state=53,
     )
 
     # Cree una instancia del modelo de regresión lineal
